0936bc5c618b06de425cd475baa2ac420b4ee010
macotok <monochromer@r8.ucom.ne.jp>
2019-09-08 19:05:52 +0900

    全てのイベントを削除するボタンをreducerで機能させる

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,13 @@ const App = () => {
     setBody('');
   };
 
+  const deleteAllEvents = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: 'DELETE_ALL_EVENTS'
+    })
+  }
+
   return (
     <div className="container-fluid">
       <h4>イベント作成フォーム</h4>
@@ -44,7 +51,7 @@ const App = () => {
           />
         </div>
         <button className="btn btn-primary" onClick={addEvent}>イベントを作成する</button>
-        <button className="btn btn-danger">全てのイベントを削除する</button>
+        <button className="btn btn-danger" onClick={deleteAllEvents}>全てのイベントを削除する</button>
       </form>
 
       <h4>イベント一覧</h4>
