2366ff5729534232b068f843cc5be8688bf9cf9c
macotok <monochromer@r8.ucom.ne.jp>
2019-09-08 13:43:43 +0900

    新規イベント作成で入力した値をreducerでstate更新まで

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,7 @@
 const events = (state = [], action) => {
   switch(action.type) {
     case 'CREATE_EVENT':
-      const event = { title: action.title, body: action.bodu };
+      const event = { title: action.title, body: action.body };
       const id = state.length ? state[state.length - 1].id + 1 : 1;
       return [...state, { id, ...event }];
     case 'DELETE_EVENT':
