3f4dda75f5cdddd072efd63daa16da3220bd8f1d
macotok <monochromer@r8.ucom.ne.jp>
2019-09-18 23:09:33 +0900

    操作ログ用のreducersを作成

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import events from './events.js';
+import operationLogs from './operationLogs';
 
 export default combineReducers({
   events,
+  operationLogs,
 })
